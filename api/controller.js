0264c16434992ad8bb65a243bf4712e1de6bcae5
Tibério César Souza do Nascimento <tiberi0@hotmail.com>
2021-03-13 15:45:36 -0300

    rodar no azure

--- api/controller.js
+++ api/controller.js
@@ -1,4 +1,3 @@
-//requires
 const mongojs = require('mongojs')
 require('dotenv/config');
 var express = require('express');
@@ -7,6 +6,7 @@ var router = express.Router();
 
 //Banco
 var connectionString = process.env.DB_URL
+console.log(connectionString)
 const db = mongojs(connectionString)
 const universal = db.collection('universal')
 
@@ -14,122 +14,122 @@ db.on('connect', function () {
   console.log('database connected')
 })
 
-//ROTAS FRONT END
 router.get('/', function(req, res, next) {
-  //console.log('Login')
-  res.sendFile(path.resolve('public/login.html'));
+    //console.log('Login')
+    res.sendFile(path.resolve('public/login.html'));
 });
 
 router.get('/forgotpassword', function(req, res, next) {
-  //console.log('Login')
-  res.sendFile(path.resolve('public/forgot-password.html'));
+    //console.log('Login')
+    res.sendFile(path.resolve('public/forgot-password.html'));
 });
-
+  
 router.get('/register', function(req, res, next) {
-  //console.log('Login')
-  res.sendFile(path.resolve('public/register.html'));
+    //console.log('Login')
+    res.sendFile(path.resolve('public/register.html'));
 });
-
+  
 router.get('/dashboard', function(req, res, next) {
-  //console.log('Login')
-  res.sendFile(path.resolve('public/dashboard.html'));
+    //console.log('Login')
+    res.sendFile(path.resolve('public/dashboard.html'));
 });
-
+  
 
 ///////////////////////////////////////////API////////////////////////////
 /* GET users listing. */
 router.get('/user/:id', function(req, res, next) {
-  //console.log(process.env.DB_URL)
-  //console.log(req.params.id)
-  universal.findOne({
-    _id: mongojs.ObjectId(req.params.id)
-  }, function(err, doc) {    
-    if (!doc) {
-      // we visited all docs in the collection
-        res.jsonp({
-          'status':'err',
-          'message':'Not found',
-          'detail':err
-        })
-        return
-    }
-    res.jsonp(doc)
-  })  
-});
-
-router.post('/api/signin', function(req,res,next){
-  if(req.body.email && req.body.pass){
+    //console.log(process.env.DB_URL)
+    //console.log(req.params.id)
     universal.findOne({
-      email: req.body.email
+      _id: mongojs.ObjectId(req.params.id)
     }, function(err, doc) {    
       if (!doc) {
         // we visited all docs in the collection
           res.jsonp({
             'status':'err',
-            'message':'Email não cadastrado!',
+            'message':'Not found',
             'detail':err
           })
           return
-      }else{
-        if(doc.pass == req.body.pass){
-          doc.pass = '';
-          res.jsonp({
-            'status':'ok',
-            'message':'',
-            'doc':doc
-          })
-        }else{
-          res.jsonp({
-            'status':'err',
-            'message':'Senha inválida'
-          })
-        }
-      }      
-    }) 
-  }else{
-    res.jsonp({
-      'status':'err',
-      'message':'Invalid Parameter'      
-    })
-  }
-})
-
-router.post('/api/signup', function(req,res,next){
-  if(req.body.email && req.body.pass && req.body.name&&req.body.last){
-    universal.findOne({
-      email: req.body.email
-    }, function(err, doc) {    
-      if (!doc) {
-        universal.insert(req.body,function(err,data){
-          if(err){
+      }
+      res.jsonp(doc)
+    })  
+  });
+  
+  router.post('/api/signin', function(req,res,next){
+    if(req.body.email && req.body.pass){
+      universal.findOne({
+        email: req.body.email
+      }, function(err, doc) {    
+        if (!doc) {
+          // we visited all docs in the collection
             res.jsonp({
               'status':'err',
-              'message':'Tente novamente mais tarde',
+              'message':'Email não cadastrado!',
               'detail':err
             })
-          }else{
-            data.pass = '';
+            return
+        }else{
+          if(doc.pass == req.body.pass){
+            doc.pass = '';
             res.jsonp({
               'status':'ok',
-              'message':'',              
-              'doc':data
+              'message':'',
+              'doc':doc
+            })
+          }else{
+            res.jsonp({
+              'status':'err',
+              'message':'Senha inválida'
             })
           }
-        })
-          
-      }else{        
-        res.jsonp({
-          'status':'err',
-          'message':'Usuário já cadastrado'
-        })        
-      }      
-    }) 
-  }else{
-    res.jsonp({
-      'status':'err',
-      'message':'Invalid Parameter'      
-    })
-  }
-})
+        }      
+      }) 
+    }else{
+      res.jsonp({
+        'status':'err',
+        'message':'Invalid Parameter'      
+      })
+    }
+  })
+  
+  router.post('/api/signup', function(req,res,next){
+    if(req.body.email && req.body.pass && req.body.name&&req.body.last){        
+      universal.findOne({
+        email: req.body.email
+      }, function(err, doc) {    
+        if (!doc) {
+          universal.insert(req.body,function(err,data){
+            if(err){
+              res.jsonp({
+                'status':'err',
+                'message':'Tente novamente mais tarde',
+                'detail':err
+              })
+            }else{
+              data.pass = '';
+              res.jsonp({
+                'status':'ok',
+                'message':'',              
+                'doc':data
+              })
+            }
+          })
+            
+        }else{        
+          res.jsonp({
+            'status':'err',
+            'message':'Usuário já cadastrado'
+          })        
+        }      
+      }) 
+    }else{
+      res.jsonp({
+        'status':'err',
+        'message':'Invalid Parameter'      
+      })
+    }
+  })
+  
 
 module.exports = router;
--- app.js
+++ app.js
@@ -4,13 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
 var apiRouter = require('./api/controller')
 
 var app = express();
 
 // view engine setup
-//app.set('view engine', 'html');
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,12 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', apiRouter);
 //app.use('/users', usersRouter);
-app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-   //next(createError(404));
-   res.sendFile(path.resolve('public/404.html'));
+  next(createError(404));
 });
 
 // error handler
@@ -35,8 +35,8 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  //res.status(err.status || 500);
-  //res.render('error');
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
